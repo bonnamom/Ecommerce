8d0ba2edd8ba1a856e6463306ca7f8558079205d
Blinkriss <mom.bonna@gmail.com>
2022-03-29 03:40:34 +0200

    homepage build

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,49 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Product;
+use App\Entity\Category;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new \Faker\Provider\Fakecar($faker));
+        $faker->addProvider(new \Bluemmb\Faker\PicsumPhotosProvider($faker));
+
+        for ($c = 0; $c < 5; $c++) {
+            $category = new Category;
+            $category
+                ->setName($faker->vehicleType)
+                ->setSlug(strtolower($this->slugger->slug($category->getName())));
+
+            $manager->persist($category);
+
+            for ($p = 0; $p < mt_rand(15, 30); $p++) {
+                $product = new Product;
+                $product->setName($faker->vehicle)
+                    ->setPrice(substr($faker->creditCardNumber(), 0, 5))
+                    ->setSlug(strtolower($this->slugger->slug($product->getName())))
+                    ->setCategory($category)
+                    ->setShortDescription($faker->paragraph())
+                    ->setMainPicture($faker->imageUrl(400, 400, true));
+
+                $manager->persist($product);
+            }
+        }
+
 
         $manager->flush();
     }
